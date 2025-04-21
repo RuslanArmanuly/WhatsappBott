f757aea4e258340e8a34cd17fcf9e2bd03c272a8
RuslanArmanuly <96491172+RuslanArmanuly@users.noreply.github.com>
2025-04-21 14:02:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import openai
 app = Flask(__name__)
 
 client = openai.OpenAI(
-    api_key="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",
+    api_key="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",
     organization="org-EGa2wJvxhFRpBMc9X8JL5ybn",
     project="proj_gbnzJRgeYifNzqBJ1M7LSJka"
 )
