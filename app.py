2f209e78c77456281e1dd8cfae77f2b859c538d4
RuslanArmanuly <96491172+RuslanArmanuly@users.noreply.github.com>
2025-04-21 14:17:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 client = openai.OpenAI(
     api_key="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",
     organization="org-EGa2wJvxhFRpBMc9X8JL5ybn",
-    project="proj_gbnzJRgeYifNzqBJ1M7LSJka"
+    project="proj_U30YgR005BETRTnmfy4ied7K"
 )
 
 @app.route("/whatsapp", methods=["POST"])
